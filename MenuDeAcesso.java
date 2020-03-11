7786cfda4684628f5f285f68888a23511db6a300
Rafael Mourão Cerqueira Figueiredo <PUCMINAS\1202488@pmg34infll21203.pucminas.net>
2020-03-11 20:17:36 -0300

    Commit 03 - Início do desenvolvimento do Menu de Acesso ( Parte 03)

---
+++ MenuDeAcesso.java
@@ -0,0 +1,62 @@
+import java.io.*;
+
+/**
+ * Classe para a implementação do MENU DE ACESSO ao sistema
+ * (Projeto CRUD - Parte 03)
+ * @author Rafael Mourão Cerqueira Figueiredo
+ * @version 1 - 11/03/2020
+ */
+class MenuDeAcesso{
+	//Atributos estáticos:
+	private static final String version = "1.0";
+	private static final String title = "AMIGO OCULTO v" + MenuDeAcesso.version;
+
+	/**
+	 * OBSERVAÇÕES:
+	 * 1 - Forma de Acesso (do usuário no sistema): através do email e senha. Se estiver cadastrado, retornar os dados. Se não, cadastrar.
+	 * 2 - Primeiro Acesso/Cadastro: o usuário deverá se cadastrar através da rotina de primeiro acesso.
+	 *   2.1 - Talvez algum usuário não chegue a se cadastrar até a data do sorteio do amigo oculto. Assim, a operação de sorteio
+	           deverá excluir da brincadeira todos aqueles que não tiverem se cadastrado.
+	 */
+
+	/**
+	 * DESAFIO 3: Crie uma terceira opção no menu de acesso que permita ao usuário cadastrar uma sua senha, caso ele tenha
+	   esquecido a atual. Para isso, você deveria simular uma operação de envio de email com uma nova senha temporária.
+	 */
+
+
+	//MÉTODOS:
+	private static void cabecalho(){ //Imprime o cabeçalho do sistema
+		System.out.println(MenuDeAcesso.title);
+		for(int i=0; i<MenuDeAcesso.title.length(); i++) System.out.print("=");
+		System.out.print("\n\n");
+	}
+
+	//ROTINAS:
+	private static void MenuPrincipal(){
+		cabecalho();
+	}
+
+	/**
+	 * Rotina para realizar o cadastro de um novo usuário
+	 */
+	private static void NovoUsuario(){
+		/**
+		 * PASSO A PASSO:
+		 * 1) Solicitar o email do novo usuário;
+		 *   1.1) Se o email informado estiver em branco (nenhum valor preenchido), retornar ao menu de acesso;
+		 * 2) Buscar se um usuário com esse email já existe, por meio do método read(email) do CRUD;
+		 * 3) Se o email já existir para algum usuário,
+		 *   3.1) Apresentar mensagem de email já cadastrado;
+		 *   3.2) Voltar ao passo 1 do laço principal;
+		 * 4) Se o email não existir,
+		 *   4.1) Solicitar o nome do usuário;
+		 *   4.2) Solicitar a senha do usuário;
+		 *   4.3) Solicitar a confirmação da inclusão do novo usuário com esses dados;
+		 *     4.3.1) Se o usuário não confirmar a inclusão, voltar ao menu de acesso;
+		 *   4.4) Incluir o usuário no arquivo, por meio do método create() do CRUD;
+		 *   4.5) Apresentar mensagem de confirmação da inclusão;
+		 *   4.6) Voltar ao menu de acesso.
+		 */
+	}
+}
